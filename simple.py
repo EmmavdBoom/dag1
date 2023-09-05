90ca5e311c04af0d71f689962deae1dcf648f3fb
Emma van den Boom <e.vandenboom@workingtalent.nl>
2023-09-05 16:21:06 +0200

    Aanpassingen

--- simple.py
+++ simple.py
@@ -1,6 +1,7 @@
 # Commentaar
 
 print("Hello world!")
+print("Dit is voor git")
 
 def funct1():
     print('Test')
@@ -8,7 +9,7 @@ def funct1():
 funct1()
 
 def funct2(tekst):
-    print('Ik print hier ' + tekst)
+    print('Ik print 1234 hier ' + tekst)
 
 funct1()
 funct2('Mijn tekst')
